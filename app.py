b5cee94baf1b30401c3debd80337bbc8b5c30e23
himansh <himanshuchoudhary1518@gmail.com>
2026-06-12 22:53:26 +0530

    Update app.py

--- app.py
+++ app.py
@@ -136,7 +136,7 @@ def create_print_job():
         # Cost
         #ppp
         cost_per_page = 1.5 if print_type == "double" else 1
-        total_cost = round(((pages + 1)//2)*cost_per_page if print_type=="double" else pages*cost_per_page, 2)
+        total_cost = round(pages * cost_per_page, 2)
         # ✅ FIXED PRINT ID
         def generate_print_id():
             while True:
@@ -376,15 +376,7 @@ def get_cost():
         #ppp
         cost_per_page = 1.5 if print_type == "double" else 1
 
-        if print_type == "double":
-            
-            if pages % 2 == 0:
-                total_cost = round((pages // 2) * cost_per_page, 2)
-            else:
-                total_cost = round(((pages // 2) + 1) * cost_per_page, 2)
-        else:
-            
-            total_cost = round(pages * cost_per_page, 2)
+        total_cost = round(pages * cost_per_page, 2)
 
         return jsonify({
             "success": True,
@@ -501,13 +493,7 @@ def upload_file():
         #ppp
         cost_per_page = 1.5 if print_type == "double" else 1
 
-        if print_type == "double":
-            if pages % 2 == 0:
-                total_cost = round((pages // 2) * cost_per_page, 2)
-            else:
-                total_cost = round(((pages // 2) + 1) * cost_per_page, 2)
-        else:
-            total_cost = round(pages * cost_per_page, 2)
+        total_cost = round(pages * cost_per_page, 2)
 
         # 5️⃣ Save job in MongoDB
         job_id = str(random.randint(100000, 999999))
